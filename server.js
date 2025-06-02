a5ff275a9b68a0840630695f6fac10987e4c23e3
madhuri-9182 <madhurisomannagari@gmail.com>
2025-06-02 15:53:12 +0530

    url

--- server.js
+++ server.js
@@ -294,5 +294,5 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log("Server running at http://localhost:8080");
 });
